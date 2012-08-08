6cacd886d5014f2a41c5873b90695ce6176f3612
Igor-Palaguta <igor.palaguta@gmail.com>
2012-08-08 17:00:29 +0300

    priority integration

---
+++ lib/AsyncDispatcher/AsyncDispatcher/ADOperationPriority.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+typedef unsigned long ADOperationPriority;
+
+extern const ADOperationPriority ADOperationPriorityDefault;
+extern const ADOperationPriority ADOperationPriorityLow;
+extern const ADOperationPriority ADOperationPriorityHigh;
+extern const ADOperationPriority ADOperationPriorityBackground;
+
+
---
+++ lib/AsyncDispatcher/AsyncDispatcher/ADOperationPriority.m
@@ -0,0 +1,6 @@
+#import "ADOperationPriority.h"
+
+const ADOperationPriority ADOperationPriorityDefault     = DISPATCH_QUEUE_PRIORITY_DEFAULT;
+const ADOperationPriority ADOperationPriorityLow         = DISPATCH_QUEUE_PRIORITY_LOW;
+const ADOperationPriority ADOperationPriorityHigh        = DISPATCH_QUEUE_PRIORITY_HIGH;
+const ADOperationPriority ADOperationPriorityBackground  = DISPATCH_QUEUE_PRIORITY_BACKGROUND;
